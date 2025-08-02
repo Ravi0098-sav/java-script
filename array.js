6a3449bfe61f554c571d6d43eec3fdab9e166e2e
RAVI SAV <ravisav5631@gmail.com>
2025-08-03 00:47:24 +0530

    simple array

---
+++ array.js
@@ -0,0 +1,7 @@
+const product = {
+    Name: "pen",
+    Rating: 4.5,
+    Discount: 0.1,
+    price :275,
+};
+
---
+++ operator.js
@@ -0,0 +1,12 @@
+// arithmetic operators 
+let a = 10;
+let b = 20;
+
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b);
+console.log(a / b); // Division
+console.log(a % b); // Modulus
+console.log(a ** b); // Exponentiation
+console.log(a++); // Increment
+console.log(a--); // Decrement
